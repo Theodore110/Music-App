d6a3d6836a341dc306c773f25cbf734342212336
Latoure341 <tukamothupi@gmail.com>
2025-07-02 17:19:33 +0200

    Middlewares and static files

--- Backend/index.js
+++ Backend/index.js
@@ -2,14 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+//middlewares and static files
+app.use(express.static("./music-app"));
+
 //Routes
 app.get("/", (req, res) =>{
-    res.status(400).send("Supposed to be a page");
+    res.status(200).sendFile(__dirname + "./music-app/index.html");
 })
 
 app.get("/budget", (req, res)=> {
     res.status(400).send("Supposed t be a page");
 })
+
+app.get("/role", (req, res) => {
+    res.status(400).send("Supposed to be a page");
+})
+
+app.all("*", (res, req)=> {
+    res.status(404).send("Page not found");
+});
 
 //Server starting
 app.listen(PORT, ()=> {
